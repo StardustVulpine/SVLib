90e2d8aaac31bb37cdd3f98be95727fa1f1fbc54
Mikołaj Biesiada <99464311+Figusorasu@users.noreply.github.com>
2024-11-17 17:57:53 +0100

    Initial commit

---
+++ SVLib.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+
+namespace SVLib
+{
+    public class SVLib : Mod
+    {
+        
+    }
+
+    public class PlayerUtils : SVLib
+    {
+        /// <summary>
+        /// Method to check if player is standing on solid ground or not
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns>True if is on ground or false if not</returns>
+        public static bool IsOnGround(Player player)
+        {
+            // Get the tile directly below the player's feet
+            int tileX = (int)(player.position.X + player.width / 2) / 16; // Horizontal center of player
+            int tileY = (int)(player.position.Y + player.height + 1) / 16; // Bottom edge of player
+
+            // Get the tile at the calculated position
+            Tile tile = Main.tile[tileX, tileY];
+
+            // Check if the tile is active and solid (indicates ground)
+            return tile != null && tile.HasUnactuatedTile && Main.tileSolid[tile.TileType];
+        }
+    }
+}
